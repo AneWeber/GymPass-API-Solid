2ee7bbc7dee793ee43c0fdd304848261c77faae2
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-03-08 11:36:13 +0100

    prisma environment for e2e test

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,24 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    workspace: [
+      {
+        extends: true,
+        test: {
+          dir: 'src/http/controllers',
+          name: 'e2e',
+          environment: './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          dir: 'src/use-cases',
+          environment: 'node',
+          name: 'unit',
+        },
+      },
+    ],
+  },
 })
